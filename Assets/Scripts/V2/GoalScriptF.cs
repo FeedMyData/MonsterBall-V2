768f2b746ae9ec2e2b2fffe79983694e86700cbf
Aurélien Picolet <aurelien.picolet@gmail.com>
2015-05-14 11:34:23 +0200

    shake on GoalScriptF added

--- Assets/Scripts/V2/GoalScriptF.cs
+++ Assets/Scripts/V2/GoalScriptF.cs
@@ -26,6 +26,9 @@ public class GoalScriptF : MonoBehaviour {
         {
             if (!other.GetComponent<MonsterControllerF>().IsMonsterForm())
             {
+                //feedbacks
+                Camera.main.GetComponent<CameraShake>().shake(0, 0, 0);
+
                 manager.AddScore(tag);
                 //tp au centre + invul de 3 secondes
                 other.GetComponent<MonsterControllerF>().Respawn();
@@ -36,8 +39,12 @@ public class GoalScriptF : MonoBehaviour {
         {
             if (player.IsProjectionInGoal())
             {
+                //feedbacks
+                Camera.main.GetComponent<CameraShake>().shake(0, 0, 0);
+
                 manager.AddScore(tag);
                 player.Respawn();
+
             }
             else
             {
