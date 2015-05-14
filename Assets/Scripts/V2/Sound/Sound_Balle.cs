0060509dc157d2f592cb8c27f6de818078464ca4
Mouettasse <n.mahieu@hotmail.fr>
2015-05-14 13:01:25 +0200

    sound shit commit

---
+++ Assets/Scripts/V2/Sound/Sound_Balle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBall : MonoBehaviour
+{
+
+    public AudioClip voiceBall;
+    
+
+       void Start()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Canvas")
+        {
+             AudioClip.Play(voiceBall);;
+         
+        }
+
+    }
+}
