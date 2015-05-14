a2da77b594deb15641419b595db20f771cd2b478
Lhudhovik <ludovic.geraert@gmail.com>
2015-05-14 12:49:12 +0200

    Test intégration animations
    Ajout et test des animations du nivek

---
+++ Assets/Scripts/Final/Bonus.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus : MonoBehaviour {
+
+    public enum Item
+    {
+        cake
+    }
+
+    public Item bonus;
+    private bool active;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!active)
+            transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * 50);
+	}
+
+    public void Activated(Vector3 position)
+    {
+        active = true;
+        switch (bonus)
+        {
+            case Item.cake:
+                //Lache le bonus à la position du joueur
+                //sendmessage au monstre
+                gameObject.SetActive(true);
+                transform.rotation = Quaternion.identity;
+                this.transform.position = position;
+                Debug.Log(position);
+                GameControllerF.GetMonster().GetComponent<MonsterControllerF>().ChargeOnCake(transform.position);
+                
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        PlayerControllerF player = other.GetComponent<PlayerControllerF>();
+        MonsterControllerF monster = other.GetComponent<MonsterControllerF>();
+
+        if ( player != null && !active)
+        {
+            player.SetBonus(this);
+            gameObject.SetActive(false);
+        }
+
+        if (monster != null && active)
+        {
+            Destroy(gameObject);
+            monster.GoodCake();
+        }
+    }
+
+    public Sprite GetSprite()
+    {
+        return GetComponent<SpriteRenderer>().sprite;
+    }
+}
---
+++ Assets/Scripts/Final/GoalScriptF.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoalScriptF : MonoBehaviour {
+
+    private GameManagerF manager;
+
+	// Use this for initialization
+	void Start () {
+	    manager = GameControllerF.getManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Monster")
+        {
+            if (!other.GetComponent<MonsterControllerF>().IsMonsterForm())
+            {
+                manager.AddScore(tag);
+                //tp au centre + invul de 3 secondes
+                other.GetComponent<MonsterControllerF>().Respawn();
+            }
+        }
+
+        if (other.tag == "TeamBlu" || other.tag == "TeamRed")
+        {
+            if (other.GetComponent<PlayerControllerF>().IsProjectionInGoal())
+            {
+                manager.AddScore(tag);
+                other.GetComponent<PlayerControllerF>().Respawn();
+            }
+            else
+            {
+                Vector3 dirImpact;
+                if (other.transform.position.x > transform.position.x)
+                    dirImpact = new Vector3(-1, 0.1f, 0);
+                else
+                    dirImpact = new Vector3(1, 0.1f, 0);
+                other.GetComponent<PlayerControllerF>().AddImpact(dirImpact * 200);
+            }
+        }
+    }
+}
