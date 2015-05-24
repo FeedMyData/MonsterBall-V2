0eba714fa4bb8c3041ab29c645198dc1b4aa90e6
Vincent Swaenepoel <swa.vincent@gmail.com>
2015-05-24 12:20:53 +0200

    Menu opening complet

---
+++ Assets/Scripts/V2/MenuStart.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MenuStart : MonoBehaviour {
+
+    public GameObject defaultButton;
+
+    public void Start()
+    {
+        if (defaultButton != null)
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+    }
+
+    public static void Play()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+}
