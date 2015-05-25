62519580fea77e22fa6eb4c2abbf4885ffa14e2f
Lhudhovik <ludovic.geraert@gmail.com>
2015-05-25 16:48:34 +0200

    Mouvement astéroïds

---
+++ Assets/Scripts/V2/AsteroidGroupBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidGroupBehavior : MonoBehaviour {
+
+	public Vector3 speed = new Vector3(0.05f, 0f,0f);
+	[Range(-300,0f)]
+	public float leftLimit;
+	[Range(0f,300f)]
+	public float rightLimit;
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach (Transform childTransform in transform) {
+
+			childTransform.Translate( speed, Space.World);
+			if(childTransform.localPosition.x > rightLimit)
+				Replace(childTransform);
+		}
+
+
+	}
+
+	void Replace(Transform target){
+
+		target.localPosition = new Vector3 (leftLimit, target.localPosition.y, target.localPosition.z);
+		target.Rotate(new Vector3(Random.Range(-180f,180f),Random.Range(-180f,180f)));
+
+	}
+}
