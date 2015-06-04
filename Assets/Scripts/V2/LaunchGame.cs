7aeb1333fc103f2003d501c053cfa347c7750675
FeedMyData <justin.guilbert@live.fr>
2015-06-04 19:32:34 +0200

    MenuStart - WIP

---
+++ Assets/Scripts/V2/LaunchGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LaunchGame : MonoBehaviour {
+
+	public void Play()
+	{
+		Debug.Log ("caca");
+		GameObject.Find ("MBL_logo").GetComponent<Image> ().enabled = true;
+		Application.LoadLevel(1);
+	}
+
+}
