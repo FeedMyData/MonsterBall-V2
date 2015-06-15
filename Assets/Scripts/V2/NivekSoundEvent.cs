d19021c015c9aec1e134b7ca3ab1b0d0641fd8e7
Lhudhovik <ludovic.geraert@gmail.com>
2015-06-15 02:21:05 +0200

    Script et marqueurs pour son pas ajoutés
    Il faut tout de même écrire le nom de l'event Wwise dans le script et
    faire glisser ce dernier sur les objets "Nivek" de la scène ( !! pas sur
    l'objet player mais bien sur Nivek !!)

---
+++ Assets/Scripts/V2/NivekSoundEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NivekSoundEvent : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayStep(){
+
+		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("", gameObject);
+	}
+}
