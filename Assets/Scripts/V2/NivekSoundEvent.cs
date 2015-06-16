d2c6b94d945e0d607e46cdcd419af0b4ec17cafa
Lhudhovik <ludovic.geraert@gmail.com>
2015-06-16 15:39:24 +0200

    Marqueurs son sur anims fin de match ajoutés

--- Assets/Scripts/V2/NivekSoundEvent.cs
+++ Assets/Scripts/V2/NivekSoundEvent.cs
@@ -8,4 +8,23 @@ public class NivekSoundEvent : MonoBehaviour {
 
 		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
 	}
+
+
+
+	public void PlayWin(){
+		
+		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
+	}
+	public void PlayWinAlternative(){
+		
+		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
+	}
+	public void PlayLose(){
+		
+		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
+	}
+	public void PlayLoseAlternative(){
+		
+		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
+	}
 }
