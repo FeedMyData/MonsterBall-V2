7833c7fd16a05f3b2e457e75a1aff1abfd10a7cb
Aurélien Picolet <aurelien.picolet@gmail.com>
2015-06-18 15:51:28 +0200

    fix bug events win/lose
    events a bit later for win/lose to make them work

--- Assets/Scripts/V2/NivekSoundEvent.cs
+++ Assets/Scripts/V2/NivekSoundEvent.cs
@@ -7,19 +7,23 @@ public class NivekSoundEvent : MonoBehaviour {
 	public void PlayStep(){
 
 		gameObject.GetComponentInParent<SoundManager> ().PlayEvent ("SFX_Niveks_Footsteps", gameObject);
+
 	}
 
 	public void PlayWin(){
 
         gameObject.GetComponentInParent<SoundManager>().PlayEvent("Anim_EndMatch_Win", gameObject);
-	}
+	
+    }
 	public void PlayWinAlternative(){
 
         gameObject.GetComponentInParent<SoundManager>().PlayEvent("Anim_EndMatch_WinAlt", gameObject);
+
 	}
 	public void PlayLose(){
 
         gameObject.GetComponentInParent<SoundManager>().PlayEvent("Anim_EndMatch_Loose", gameObject);
+
 	}
 	public void PlayLoseAlternative(){
 
